fa1c0c099371f520212a2de413c78d64380d5fcd
Tobias Blum <tobias.blum@besonet.ch>
2023-07-16 12:31:05 +0200

    Add basic nuxt and elysia api testing

---
+++ src/nitro-nuxt/load-test.ts
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export const options = {
+  vus: 10,
+  duration: "10s",
+};
+
+export default function () {
+  const response = http.get("http://localhost:3000/api/hello");
+
+  check(response, {
+    "status is 200": (r) => r.status === 200,
+    "payload matches": (r) => r.json().hello === "nuxt",
+  });
+  // sleep(1);
+}
